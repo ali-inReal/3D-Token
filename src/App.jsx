aff6a7457bdad6a36b9507eb9e3e01210d60c944
ali-inReal <alim07922@gmail.com>
2023-04-26 11:00:37 +0500

    coin

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,24 @@
 import React, { useEffect, useRef, useState } from 'react'
-import Wheel from './wheelOfFortune/wheel'
-import icon1 from "./assets/icons/icon1.png"
-import icon2 from "./assets/icons/icon2.png"
-import icon3 from "./assets/icons/icon3.png"
-import icon4 from "./assets/icons/icon4.png"
-import icon5 from "./assets/icons/icon5.png"
-import icon6 from "./assets/icons/icon6.png"
-import logo from "./assets/logo1.png"
+import { Canvas } from '@react-three/fiber'
+import { Coin } from './Coin/Coin'
+import { OrbitControls } from '@react-three/drei'
 export const App = () => {
-    const segments = [
-        { name: 'GOODS',image:icon1 , color: "" },
-        { name:'SERVICES',image:icon2,color: "" },
-        { name: 'PROMOS', image:icon3 , color: "" },
-        { name: 'MYSTERY', image:icon4 , color: "" },
-        { name: 'ACTS', image:icon5 , color: "" },
-        { name: 'ENTERTAINMENT', image:icon6, color: "" }
-      ]
-    const [result,setResult] = useState(-1);
-    useEffect(
-        ()=>{
-        if(result != -1)
-           {
-            console.log(segments[result].name)
-           }
-           
-        },[result]
-    )
+  
     
-    const ref= useRef();
 
     return (
-    <div style={{
-      width:"70vw",
-      height:"70vh"
-    }}>
-         <Wheel  ref = {ref} setResult={setResult} logo={logo} segments={segments} flickering={false} lightOn={true} soundOn={true} textOrientation={"vertical"} textSize="8px" />
-         <button
+      <div 
         style={{
-          padding: "10px",
-          borderRadius: "10px",
-          backgroundColor: "gold",
-          color: "white",
-          border: "none",
-          cursor: "pointer"
+          width:"100vw",
+          height:"100vh"
         }}
-        onClick={() => ref.current.clickHandler()}>Spin</button>
-    </div>
+       >
+           <Canvas>                                                                  
+        
+        <Coin/>
+        <OrbitControls/>
+    </Canvas>
+      </div>
+    
   )
 }
